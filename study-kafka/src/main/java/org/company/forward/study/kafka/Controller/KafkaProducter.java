d60cf1c542b743ff2d61f54f06bf8a6f1ed5ed07
wjian17 <1274374508@qq.com>
2020-05-26 18:17:48 +0800

    commit

--- study-kafka/src/main/java/org/company/forward/study/kafka/Controller/KafkaProducter.java
+++ study-kafka/src/main/java/org/company/forward/study/kafka/Controller/KafkaProducter.java
@@ -2,6 +2,8 @@ package org.company.forward.study.kafka.Controller;
 
 import org.company.forward.domain.basic.BasicResponse;
 import org.company.forward.study.kafka.config.BindChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,16 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KafkaProducter {
 
+    private Logger logger = LoggerFactory.getLogger(KafkaProducter.class);
+
     @Autowired
     private BindChannel bindChannel;
 
     @RequestMapping("/send/{msg}")
     public BasicResponse send(@PathVariable("msg") String msg) {
+        logger.info(msg);
         return bindChannel.sendMsg(msg);
     }
 
     @RequestMapping("/feignSend")
     public BasicResponse feignSend(String msg) {
+        logger.info(msg);
         return bindChannel.sendMsg(msg);
     }
 
