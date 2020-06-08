e9cd7131086a6a921ecd3c4ee69d2cf6d315ca44
wjian17 <1274374508@qq.com>
2020-06-08 12:14:28 +0800

    commint

--- study-lcntm/src/main/java/org/company/forward/study/lcntm/StudyLcntmApplication.java
+++ study-lcntm/src/main/java/org/company/forward/study/lcntm/StudyLcntmApplication.java
@@ -1,11 +1,16 @@
 package org.company.forward.study.lcntm;
 
 import com.codingapi.txlcn.tm.config.EnableTransactionManagerServer;
+import org.company.forward.db.config.DataSourceAspect;
+import org.company.forward.db.config.DynamicDataSourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@Import(value = {DynamicDataSourceConfig.class, DataSourceAspect.class})
 @EnableDiscoveryClient
 @EnableTransactionManagerServer
 public class StudyLcntmApplication {
