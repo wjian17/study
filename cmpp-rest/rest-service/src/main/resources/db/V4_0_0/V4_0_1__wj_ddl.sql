6ff4f6a202347e4c8657e398e7b22b33d0f6e70c
wjian17 <1274374508@qq.com>
2021-12-30 17:02:19 +0800

    flyway测试

---
+++ cmpp-rest/rest-service/src/main/resources/db/V4_0_0/V4_0_1__wj_ddl.sql
@@ -0,0 +1,7 @@
+use #{rest};
+
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL,
+  `name` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ cmpp-rest/rest-service/src/main/resources/db/V4_0_0/V4_0_2__wj_dml.sql
@@ -0,0 +1,15 @@
+use #{rest};
+
+
+
+INSERT INTO `test` (`id`, `name`)
+VALUES
+  (44, '3');
+
+INSERT INTO `test` (`id`, `name`)
+VALUES
+  (55, '3');
+
+INSERT INTO `test` (`id`, `name`)
+VALUES
+  (66, '3');
